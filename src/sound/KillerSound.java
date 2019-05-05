c8344eee916a3d7028a85386bbf722d69f1a2f97
Christian Campos <32370442+christiancampos123@users.noreply.github.com>
2019-05-05 19:19:06 +0200

    Update KillerSound.java

--- src/sound/KillerSound.java
+++ src/sound/KillerSound.java
@@ -10,5 +10,95 @@ package sound;
  * @author Javi
  */
 public class KillerSound {
+        static int i = 0;
+    // Songs
+    static Clip soundBattleSong = getSound("battle.wav");
+    static Clip soundMenuSong = getSound("manu.wav");
+    static Clip soundEndingSong = getSound("ending.wav");
+
+    // Shots
+    static Clip soundShot = getSound("shot.wav");
+    static Clip soundRocket = getSound("cohete.wav");
+
+    // Skills
+    static Clip soundNitro = getSound("nitro.wav");
+    static Clip soundDash = getSound("salto.wav");
     
+
+    // Explosion
+    static Clip soundExplosion = getSound("explosion.wav");
+
+    // PacMan
+    static Clip soundPacManDie = getSound("PacManDie.wav");
+    static Clip soundPacManMove = getSound("PacManMove.wav");
+    static Clip soundPacManEat = getSound("PacManEat.wav");
+    
+    //Clicks
+    static Clip soundclicMobile = getSound("mobileClic.wav");
+    static Clip soundclicPC = getSound("clicPc.wav");
+
+    // Collision
+    static Clip sound2 = getSound("menu.wav");
+    static Clip sound = getSound("nitro.wav");
+    
+    // Dead
+    static Clip soundDieGTA = getSound("deadGTA.wav");
+    static Clip soundDieDark = getSound("deadSouls.wav");
+    
+    //Dark Hole
+    static Clip soundTp = getSound("tp.wav");
+    
+    //powerUp
+    static Clip soundPowerUp = getSound("powerUp.wav");
+    
+    // Collision
+    
+
+    
+    
+    static boolean stopLoop = false;
+    static int algo = 0;
+    //static Clip sound2 = getSound("battle.wav");
+
+    public static Clip getSound(String file) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\Christian\\Desktop\\editados 2\\" + file));
+            AudioFormat format = audioInputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            Clip sound = (Clip) AudioSystem.getLine(info);
+            sound.open(audioInputStream);
+            return sound;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static void playSound(Clip clip) {
+        clip.stop();
+        clip.setFramePosition(0);
+        clip.start();
+    }
+
+    public static void stopSound(Clip clip) {
+        clip.stop();
+    }
+
+    public static void loopSound(Clip clip) {
+        //playSound(clip);
+        
+            //if (auxSound == true) {
+            if (algo == 0){
+                playSound(clip);
+                
+                algo ++;
+            }
+            if (clip.getFrameLength() == clip.getFramePosition()) {
+                playSound(clip);
+            } else {
+                //stopSound(clip);
+            
+            //}
+        }
+    }
+
 }
