dcb7a7aa628adadd5838aff998cfa625648a5891
Alvaro <alvarocabreradam2@gmail.com>
2019-05-11 12:08:48 +0200

    Metas trello 20190511

--- src/game/KillerGame.java
+++ src/game/KillerGame.java
@@ -4,17 +4,24 @@ package game;
 import visualEffects.*;
 import visibleObjects.*;
 import communications.*;
+import gameRoom.KillerPanel;
 // Other imports
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.net.Socket;
 import java.util.ArrayList;
 import javax.swing.*;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 /**
  * @author Alvaro
  */
 public class KillerGame extends JFrame {
+
+    public void addKillerShoot(KillerShip aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     // Attributes
     
@@ -27,7 +34,7 @@ public class KillerGame extends JFrame {
     
     // Lista de objetos
     private ArrayList<VisibleObject> objects = new ArrayList<>();
-    
+
     // Lista de naves de naves | IP del mando al que pertenece, Nave
     private Hashtable<String, Controlled> ships = new Hashtable();
     
@@ -47,68 +54,47 @@ public class KillerGame extends JFrame {
     // Constructors 
     public KillerGame() {
         
-        // Update game status
-        this.status = Status.room;
+        // Set game status
+        // this.status = Status.room;
+        
+        // Show window
+        this.showWindow();
         
         // Open left and right modules
         this.newPrevModule();
-        this.newPrevModule();
+        this.newNextModule();
         
         // Open server
         this.newServer();
         
-        // Show window
+        // Show room
+        // this.newKillerRoom();
         
-        // Start room
+        // Start game
+        this.startGame();
+        
+        // Add walls
+        this.newWall(Wall.Limit.EAST);
+        this.newWall(Wall.Limit.NORTH);
+        this.newWall(Wall.Limit.SOUTH);
+        this.newWall(Wall.Limit.WEST);
         
     }
 
     // Methods
-    public void checkColision(Alive obj) {
+    public void checkColision(Alive alive) {
         
-        // TODO
-
-        // Bernat Code //  Este codigo sera eliminado //
-        for (int i = 0; i < objects.size(); i++) {
-
-            VisibleObject objCol = null;
-
-            if (objects.get(i) != obj) {
-                objCol = objects.get(i);
+        for (int inc = 0; inc < this.objects.size(); inc ++) {
+            
+            if ( false ) { // Aqui no se que condicion poner para detectar si se chocan, tengo que hablar con Maria
+                
+                KillerRules.collision(alive, this.objects.get(inc));
+                
             }
-
-            if (objCol instanceof Alive) {
-                if (obj.nextMove().intersects(((Alive) objCol).hitbox)) {
-                    KillerRules.collision(obj, objCol);
-                }
-
-            }
-
+            
         }
-        // Bernat Code // Este codigo sera eliminado //
         
     }
-
-    // Bernat Code // Este metodo sera eliminado //
-    public void checkColision(Shoot obj) {
-        //colision con limites
-        if (obj.getY() >= viewer.getHeight() || obj.getY() <= 0
-                || obj.getX() >= viewer.getWidth() || obj.getX() <= 0) {
-            obj.death();
-        }
-        for (int i = 0; i < objects.size(); i++) {
-            VisibleObject objCol = null;
-            if (objects.get(i) != obj.getControlled()) {
-                objCol = objects.get(i);
-            }
-            if (objCol instanceof Alive) {
-                if (obj.hitbox.intersects(((Alive) objCol).hitbox)) {
-                    KillerRules.collisionShoot(obj, (Alive) objCol);
-                }
-            }
-        }
-    }
-    // Bernat Code // Este metodo sera eliminado //
     
     public void sendMessageToPrev(Message message) {
         // TODO
@@ -126,15 +112,43 @@ public class KillerGame extends JFrame {
         // TODO
     }
     
-    public void StartGame() {
+    public void sendReady() {
+        this.nextModule.startGame();
+        /*/
         if (this.status == Status.room) {
             this.status = Status.starting;
-            // TODO
-            this.status = Status.game;
+            
         }
+        /*/
+        System.out.println("Ready");
+    }
+    
+    public void startGame() {
+        
+        /*/
+        if (this.status == Status.starting) {
+            this.status = Status.starting;
+            this.nextModule.startGame();
+        }
+        /*/
+        
+        // TODO Empezar Juego
+        this.status = Status.starting;
+        this.newViewer();
+    }
+    
+    public void showWindow() {
+        this.setSize(1120, 630);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setLayout(new GridLayout());
+        this.setResizable(false);
+        this.setVisible(true);
     }
     
     // Methods new
+    
+    // New communications
     private void newServer() {
         try {
             this.server = new KillerServer(this, 8000);
@@ -158,36 +172,47 @@ public class KillerGame extends JFrame {
         
         boolean result = false;
         
-        if (this.status == Status.room) {
-            KillerPad pad = new KillerPad(this, socket, user, color);
+        // if (this.status == Status.room) {
+            KillerPad pad = new KillerPad(this, socket, user, color, ip);
             this.pads.put(ip, pad);
             new Thread(pad).start();
             result = true;
-        }
+        // }
         
         return result;
         
     }
-
-    public void newKillerShip(String ip, Color color, String user) {
-        Controlled ship = new Controlled(this, color, ip, user);
+    
+    // New VisibleObjects
+    public void newKillerShip(String ip, Color color, String user, int x, int y, KillerShip.ShipType type) {
+        KillerShip ship = new KillerShip(this, x, y, ip, user, type);
         this.ships.put(ip, ship);
         this.objects.add(ship);
         new Thread(ship).start();
     }
     
-    public void newShoot(Color color, Controlled ship) {
-        Shoot shoot = new Shoot(this, color, ship);
+    public void newShoot(KillerShip ship) {
+        Shoot shoot = new Shoot(this, ship);
         this.objects.add(shoot);
         new Thread(shoot).start();
     }
-
-    // Este metodo sera eliminado //
-    public void createControlled(Controlled contr) {
-        objects.add(contr);
-        new Thread(contr).start();
+    
+    public void newWall(Wall.Limit limit) {
+        Wall wall = new Wall(this, limit);
+        this.objects.add(wall);
     }
-    // Este metodo sera eliminado //
+    
+    // New room
+    public void newKillerRoom() {
+        KillerPanel panel = new KillerPanel(this);
+    }
+    
+    // New Viewer
+    private void newViewer() {
+        this.viewer = new Viewer(this);
+        this.add(this.viewer, 0, 0);
+        new Thread(this.viewer).start();
+    }
 
     // Methods get    
     public KillerPad getPadByIP(String ip) {
@@ -206,19 +231,11 @@ public class KillerGame extends JFrame {
         return this.server;
     }
     
-    /**
-     * El nombre de este metodo sera cambiado por getPrevModule()
-     * @return Modulo previo.
-     */
-    public VisualHandler getPk() {
+    public VisualHandler getPrevModule() {
         return this.prevModule;
     }
     
-    /**
-     * El nombre de este metodo sera cambiado por getNextModule()
-     * @return Modulo siguiente.
-     */
-    public VisualHandler getNk() {
+    public VisualHandler getNextModule() {
         return this.nextModule;
     }
     
@@ -230,11 +247,30 @@ public class KillerGame extends JFrame {
         return this.status;
     }
     
+    public ArrayList<VisibleObject> getObjects() {
+        return this.objects;
+    }
+    
     // Methods set
     
     // Main activity
     public static void main(String[] args) {
         KillerGame game = new KillerGame();
+        
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter IP");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("IP to connect is: " + userName);  // Output user input
+
+        System.out.println("Enter PORT");
+
+        int port = Integer.parseInt(myObj.nextLine());  // Read user input
+        System.out.println("IP to connect is: " + port);  // Output user input ¡
+        
+        game.getNextModule().setDestinationPort(port);
+        game.getNextModule().setDestinationIp(userName);
+        
     }
 
 }
--- src/game/KillerRules.java
+++ src/game/KillerRules.java
@@ -1,81 +1,121 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
 
-import visibleObjects.Automata;
-import visibleObjects.VisibleObject;
-import visibleObjects.Alive;
-import visibleObjects.Shoot;
-import visibleObjects.Controlled;
+import visibleObjects.*;
 
 /**
  *
- * @author berna
+ * @author Alvaro
  */
 public class KillerRules {
 
-    public static void collision(VisibleObject obj1, VisibleObject obj2) {
-        if (obj1 instanceof Alive && obj2 instanceof Alive) {
-            collisionAlive((Alive) obj1, (Alive) obj2);
+    public static void collision(Alive alive, VisibleObject object) {
+        
+        if (alive instanceof KillerShip) {
+            KillerRules.collisionShip((KillerShip) (alive), object);
         }
+        
+        if (alive instanceof Shoot) {
+            KillerRules.collisionShoot((Shoot) (alive), object);
+        }
+        
     }
 
-    public static void collisionAlive(Alive obj1, Alive obj2) {
-
-//        obj1.collision();
-//        obj2.collision();
-        if (obj1 instanceof Controlled && obj2 instanceof Controlled) {
-
-            if (!((Controlled) obj1).isDeath() && !((Controlled) obj2).isDeath()) {
-//                ((Controlled) obj2).kill();
-//                ((Controlled) obj1).kill();
-//                System.out.println("muertos");
-//            } else {
-//                if (!((Controlled) obj1).isDeath() && ((Controlled) obj1).isDeath()) {
-//                    System.out.println("muerto1");
-//                    ((Controlled) obj2).kill();
-//                }
-//                if (!((Controlled) obj2).isDeath() && !((Controlled) obj1).isDeath()) {
-//                    System.out.println("muerto2");
-//                    ((Controlled) obj1).kill();
-//                }
-            }
-
-        } else  {
-            if (obj1 instanceof Controlled && obj2 instanceof Automata
-                    && !((Controlled) obj1).isDeath()) {
-                ((Controlled) obj1).kill();
-                obj1.death();
-            }
-
-            if (obj2 instanceof Controlled && obj1 instanceof Automata
-                    && !((Controlled) obj2).isDeath()) {
-                ((Controlled) obj2).kill();
-                obj1.death();
-            }
-
+    private static void collisionShip(KillerShip killerShip, VisibleObject object) {
+        
+        // Collision with Wall
+        if (object instanceof Wall) {
+            
+        }
+        
+        // Collision with Ship
+        if (object instanceof KillerShip) {
+            
+        }
+        
+        // Collision with Asteroid
+        if (object instanceof Asteroid) {
+            
+        }
+        
+        // Collision with BlackHole
+        if (object instanceof BlackHole) {
+            
         }
 
+        // Collision with Nebulosa
+        if (object instanceof Nebulosa) {
+            
+        }
+        
+        // Collision with Pacman
+        if (object instanceof Pacman) {
+            
+        }
+        
+        // Collision with Planeta
+        if (object instanceof Planeta) {
+            
+        }
+        
+        // Collision with PowerUp
+        if (object instanceof PowerUp) {
+            
+        }
+        
+        // Collision with PowerUp
+        if (object instanceof Shoot) {
+            
+        }
+        
     }
 
-    public static void collisionShoot(Shoot obj1, Alive obj2) {
-
-        if (obj2 instanceof Controlled && !((Controlled) obj2).isDeath()) {
-            System.out.println(obj1.getShip().getIp()
-            +"/"+((Controlled) obj2).getIp());
-            if (!obj1.getShip().getIp().equals(((Controlled) obj2).getIp())) {
-                ((Controlled) obj2).kill();
-                obj1.points(5);
-            }
-        } else if (obj2 instanceof Automata) {
-            obj2.death();
-            obj1.points(2);
+    private static void collisionShoot(Shoot shoot, VisibleObject object) {
+        
+        // Collision with Wall
+        if (object instanceof Wall) {
+            
+        }
+        
+        // Collision with Ship
+        if (object instanceof KillerShip) {
+            
+        }
+        
+        // Collision with Asteroid
+        if (object instanceof Asteroid) {
+            
+        }
+        
+        // Collision with BlackHole
+        if (object instanceof BlackHole) {
+            
         }
 
-        obj1.death();
+        // Collision with Nebulosa
+        if (object instanceof Nebulosa) {
+            
+        }
+        
+        // Collision with Pacman
+        if (object instanceof Pacman) {
+            
+        }
+        
+        // Collision with Planeta
+        if (object instanceof Planeta) {
+            
+        }
+        
+        // Collision with PowerUp
+        if (object instanceof PowerUp) {
+            
+        }
+        
+        // Collision with PowerUp
+        if (object instanceof Shoot) {
+            
+        }
+        
     }
 
 }
