6c93e21258e1ad5c702572f00258a5574a9f1d13
Maria Adrover <maria.adrover.v@gmail.com>
2019-06-04 19:14:21 +0200

    added new img files, Planeta: random img, Powerup changed route

--- src/visibleObjects/Planeta.java
+++ src/visibleObjects/Planeta.java
@@ -3,14 +3,14 @@ package visibleObjects;
 import game.KillerGame;
 
 public class Planeta extends Static {
-    
+
     /**
-     * 
+     *
      * @param game
      * @param x
      * @param y
      * @param height
-     * @param m 
+     * @param m
      */
     public Planeta(KillerGame game, double x, double y, int height) {
         super(game, x, y);
@@ -26,15 +26,17 @@ public class Planeta extends Static {
     // ********************************************************
     // *                     Interfaces                       *
     // ********************************************************
-    
-    
-
     // *********************
     // * Getters & Setters *
     // *********************
-
     @Override
     protected void setImage() {
-        this.loadImg("src/visibleObjects/img/planet1.png");
+        int s = (int)(Math.random()* 2 + 1);
+        System.out.println("Planeta " + s);
+        if (s == 1) {
+            this.loadImg("src/visibleObjects/img/planeta1.png");
+        } else {
+            this.loadImg("src/visibleObjects/img/planeta2.png");
+        }
     }
 }
